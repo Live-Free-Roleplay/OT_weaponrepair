16956f3d4bb40a101f563606cba2c8bfb220bdf6
Clink123 <clink123gamer@gmail.com>
2024-03-16 01:37:02 -0400

    Add local config

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.useOTSkills = false -- requires our skills system, you can find here: https://forum.cfx.re/t/paid-ot-skills/4917372
+Config.useOTSkills = true -- requires our skills system, you can find here: https://forum.cfx.re/t/paid-ot-skills/4917372
 Config.xpreward = 5
 
 Config.requireditem = 'money'
@@ -9,18 +9,24 @@ Config.repairtime = 5000
 Config.repairItem = 'weaponrepairkit'
 
 Config.require = {
-    ['WEAPON_COMBATPDW'] = {
+    --[[['WEAPON_COMBATPISTOL'] = {
         requireditem = 'money',
         requireditemamount = 2000,
         repairtime = 10000
-    },
+    },--]]
 }
 
 Config.locations = {
     {
-        coords = vector3(-567.8292, -1696.3129, 19.0366),
-        heading = 210.3678,
-        spawnprop = true, -- spawns the workbench at the location 
-        free = false -- allows weapons to be repaired for free at the location
+        coords = vector3(487.34, -997.11, 30.69),
+        heading = 268.7,
+        spawnprop = false, -- spawns the workbench at the location 
+        free = true -- allows weapons to be repaired for free at the location
+    },
+    {
+        coords = vector3(1838.4341, 3686.2830, 34.1892),
+        heading = 297.7212,
+        spawnprop = false, -- spawns the workbench at the location 
+        free = true -- allows weapons to be repaired for free at the location
     }
 }
